3f49a5c5115c86f9b2f63e4af86c3ef96fe4247d
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-07 15:19:04 -0500

    set up routers in htmlRoutes.js and apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const fs = require('fs');
+const db = require("../db/db.json");
+
+router.get("/api/notes", (req, res) => {
+    
+})
+
+//post
+
+//delete
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +13,6 @@ app.use(express.static('public'));
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {;
+app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} `)
-})
+});
