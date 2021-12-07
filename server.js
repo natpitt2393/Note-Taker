7aad62b41d89c66bbba53570588a906483150637
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-07 08:30:05 -0500

    added app.listener, app.use for apiroutes, and created dependencies for htmlroutes.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {;
+    console.log(`App listening at http://localhost:${PORT} `)
+})
