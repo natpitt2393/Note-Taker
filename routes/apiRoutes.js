b30150708f5a7144e1d5c6a1147bdb99ae054a40
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-07 17:57:42 -0500

    router in htmlRoutes.js can now access notes.html and index.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
 const fs = require('fs');
-const db = require("../db/db.json");
-
-router.get("/api/notes", (req, res) => {
+const dbFile = require("../db/db.json");
+//get notes
+router.get("/notes", (req, res) => {
+    // let notes = fs.readFileSync("./db/db.json").then((notes) => {
+    //     let jsonNotes = JSON.parse(notes)
+    //     console.log(notes)
+    //as of right now the dbFile is not displaying on the page
+    let notes = JSON.parse(dbFile)
+    console.log(notes);
+        return res.json(notes);
+    });
     
+
+
+//post a note
+router.post("api/notes", (req, res) => {
+    res.json(dbFile);
 })
-
-//post
-
 //delete
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
+
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} `)
