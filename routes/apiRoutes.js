bf4e312e9a08116ee8931e2a72fb9000a361cc80
Nat Epstein <nathaniel.epstein@gmail.com>
2021-12-07 20:50:02 -0500

    notes now are pushed to array and save using writefile method with fs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,38 @@
 const router = require('express').Router();
 const fs = require('fs');
 const dbFile = require("../db/db.json");
+
 //get notes
 router.get("/notes", (req, res) => {
-    // let notes = fs.readFileSync("./db/db.json").then((notes) => {
-    //     let jsonNotes = JSON.parse(notes)
-    //     console.log(notes)
-    //as of right now the dbFile is not displaying on the page
-    let notes = JSON.parse(dbFile)
-    console.log(notes);
-        return res.json(notes);
-    });
-    
-
+    console.log(dbFile);
+    return res.json(dbFile);
+});
 
 //post a note
-router.post("api/notes", (req, res) => {
-    res.json(dbFile);
+// listening for someone to post to the url
+router.post("/notes", (req, res) => {
+
+
+    dbFile.push(req.body);
+    // get the info from the post (req.body)
+     // an object
+    console.log(dbFile) // and array of objects
+    fs.writeFile('./db/db.json', JSON.stringify(dbFile), err => {
+        if (err) {
+            console.log(err);
+        }
+        return res.json(dbFile);
+    })
+
+    // update the json file 
+    // -- update our local variable (dbFile) (add the new obj to the array of obj)
+    // -- overide the json file w/ the updated variable 
+    // -- -- (fsWriteFile)
+
+    // end the response or send back the modified obj
+    // res.end()
 })
+
 //delete
 
 module.exports = router;
